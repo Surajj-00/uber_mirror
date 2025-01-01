14ef661e14aef928a73ed603821f8c3bca14d1a4
Suraj Sahu <surajsahu61771@gmail.com>
2025-01-02 01:38:26 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes')
 
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
   res.send('Your Backend is Succesfully Setup🎉🎉')
 })
+
+app.use('/users', userRoutes);
 
 
 module.exports = app;
